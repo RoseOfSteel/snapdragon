cd62bd3373d6d0f8e25fdb8f8b6c0a93a7e52a24
RoseOfSteel <roseofsteeldev@gmail.com>
2024-01-06 14:55:46 -0600

    Clean up root_path file and add new line to gitignore

--- root_path.py
+++ root_path.py
@@ -1,7 +1,11 @@
+"""
+Store important paths.
+"""
 import os
 from pathlib import Path
 
-import os
+# Path to this specific file (so we can always access the project root directory)
 root_path = os.path.dirname(os.path.realpath(__file__))
 
+# Path to the database file
 db_path = root_path + '\snapdragon.db'
