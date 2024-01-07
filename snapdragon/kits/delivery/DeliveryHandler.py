31fbc8783485e5ee3e0dae7ef1639a17494c3439
RoseOfSteel <149306708+RoseOfSteel@users.noreply.github.com>
2024-01-06 22:58:02 -0600

    Set up Delivery Handler (#5)
    * Add the Delivery Handler to return delivery instructions. Update DatabaseManager to retrieve delivery instructions from the database. Add a helper method to clean up list of lists.
    
    * Remove Todo comment

---
+++ snapdragon/kits/delivery/DeliveryHandler.py
@@ -0,0 +1,39 @@
+from snapdragon.utils.DatabaseManager import DatabaseManager
+from root_path import db_path
+
+"""
+This class manages functions needed to manage product delivery instructions.
+"""
+class DeliveryHandler:
+    """
+    Constructor.
+    """
+    def __init__(self):
+        pass
+
+    """
+    Retrieve the instructions to deliver a particular product.
+
+    name(str): Name of the object you need instructions for.
+    software(bool): True if you want the software deliverable instructions. False if not.
+    hardware(bool): True if you want the hardware deliverable instructions. False if not.
+    documentation(bool): True if you want the documentation deliverable instructions. False if not.
+
+    return: List with instructions retrieved from the database.
+    """
+    def retrieve_delivery_instructions(self, name, software, hardware, documentation):
+        dm = DatabaseManager(db_path)
+        dm.connect()
+        result = dm.query_delivery_instructions(name, software, hardware, documentation)
+        dm.close()
+        return result
+
+# Test the Delivery Handler
+delivery_handler = DeliveryHandler()
+result = delivery_handler.retrieve_delivery_instructions("cubesat", True, True, False)
+print(result)
+
+delivery_handler = DeliveryHandler()
+result = delivery_handler.retrieve_delivery_instructions("AI Software", True, True, True)
+print(result)
+    
